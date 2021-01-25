67e2d48c6a5c9539177d888723889602a877f92e
unknown <costa.sergio.86@gmail.com>
2021-01-25 20:10:02 +0000

    Installation + Article Creation

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '31b578fab3fa700bb620e280d73bbf44'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '160e1cb7-f3fc-4a5b-9ac4-24bbc507a7f5'
+};
